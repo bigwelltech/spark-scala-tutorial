9d26d116eb30ce88f99548aeed4c3c5385708266
BigWellTech <bigwelltech@gmail.com>
2021-07-02 18:14:25 +0530

    How to create rdd by textfile

--- src/main/scala/com/bigwelltech/spark/Main.scala
+++ src/main/scala/com/bigwelltech/spark/Main.scala
@@ -3,13 +3,23 @@ package com.bigwelltech.spark
 import org.apache.spark.sql.SparkSession
 
 object Main {
+  def getRddByCollect(sparkSession: SparkSession) {
+    val rdd = sparkSession.sparkContext.parallelize(Seq("one", "two", "three"))
+    rdd.foreach(f => println(f))
+    println(rdd.count())
+  }
+  def getRddByTextFile(sparkSession: SparkSession) {
+    val rdd = sparkSession.sparkContext.textFile("src/main/resources/test.txt")
+    rdd.foreach(f => println(f))
+    println(rdd.count())
+  }
+
   def main(args: Array[String]): Unit = {
     println("**Start**")
     System.setProperty("hadoop.home.dir", "D:/bigwelltech/sparkconf/conf/")
     val sparkSession = SparkSession.builder().appName("Spark-Scala_Tutorial").master("local").getOrCreate()
-    val rdd = sparkSession.sparkContext.parallelize(Seq("one", "two", "three"))
-    rdd.foreach(f => println(f))
-    println(rdd.count())
+    //getRddByCollect(sparkSession)
+    getRddByTextFile(sparkSession)
     println("**End**")
   }
 }
