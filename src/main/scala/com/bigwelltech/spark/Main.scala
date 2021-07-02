cf05879cd529db5c522bb9b418aa816cac2f5436
BigWellTech <bigwelltech@gmail.com>
2021-07-02 18:42:35 +0530

    map and flatmap

--- src/main/scala/com/bigwelltech/spark/Main.scala
+++ src/main/scala/com/bigwelltech/spark/Main.scala
@@ -14,12 +14,28 @@ object Main {
     println(rdd.count())
   }
 
+  def mapAndFlatMap(sparkSession: SparkSession) {
+    //map ->one to one
+    //flat -> one to many
+    val rdd = sparkSession.sparkContext.textFile("src/main/resources/test.txt")
+    val maprdd = rdd.map(x => (x, x.length()))
+    maprdd.foreach(f => println(f))
+    println(maprdd.count())
+    println("****FlatMap*****")
+    val flatmap=rdd.flatMap(x=>x.split(" "))
+    val myrdd=flatmap.map(f=>(f,f.length()))
+    myrdd.foreach(x=>println(x))
+    println(myrdd.count())
+
+  }
+
   def main(args: Array[String]): Unit = {
     println("**Start**")
     System.setProperty("hadoop.home.dir", "D:/bigwelltech/sparkconf/conf/")
     val sparkSession = SparkSession.builder().appName("Spark-Scala_Tutorial").master("local").getOrCreate()
     //getRddByCollect(sparkSession)
-    getRddByTextFile(sparkSession)
+    //getRddByTextFile(sparkSession)
+    mapAndFlatMap(sparkSession)
     println("**End**")
   }
 }
